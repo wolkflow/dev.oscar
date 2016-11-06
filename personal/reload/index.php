705be5b27951f547177879e368faea59fea14618
root <root@vps-1045602-16500.host4g.ru>
2016-11-06 05:06:28 +0300

    Picture reload.

---
+++ personal/reload/index.php
@@ -0,0 +1,23 @@
+<? define('PAGE', 'UPLOAD') ?>
+<? require ($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php'); ?>
+<? $APPLICATION->SetTitle("Загрузка изображения"); ?>
+
+<?  // Строка поиска.
+	$APPLICATION->IncludeComponent('bitrix:main.include', '', array(
+		'AREA_FILE_SHOW' => 'file',
+		'PATH' => SITE_TEMPLATE_PATH.'/include/area/main.search.php',
+		'EDIT_TEMPLATE' => 'html'
+	));
+?>
+
+<main class="siteMain bg-gray">
+	<?	// Загрузка изображения.					
+		$APPLICATION->IncludeComponent(
+			"glyf:picture.upload",
+			"picture",
+			array("PID" => intval($_REQUEST['PID']))
+		);
+	?>
+</main>
+
+<? require ($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php'); ?>
