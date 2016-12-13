11926ccbd3ca0f1877d81b3da3c056338f25afeb
root <root@vps-1045602-16500.host4g.ru>
2016-12-13 22:46:12 +0300

    Translates.

--- local/components/glyf/user.picture/templates/picture/lang/en/template.php
+++ local/components/glyf/user.picture/templates/picture/lang/en/template.php
@@ -1,3 +1,29 @@
 <?php
 
-$MESS['GL_NO_FOUND'] = 'Sorry, picture not found.';
+$MESS['GL_NO_FOUND'] = 'Unfortunately, the image is not found.';
+$MESS['GL_TYPE_AND_SIZE'] = 'The type and size of the image';
+$MESS['GL_EDIT'] = 'Edit';
+$MESS['GL_PERSONAL_ACCOUNT'] = 'Personal account';
+$MESS['GL_DIRECTORY'] = 'Catalog';
+$MESS['GL_VIEWS'] = 'views';
+$MESS['GL_VIEWS_2'] = 'Views';
+$MESS['GL_SALES'] = 'sales';
+$MESS['GL_SALES_2'] = 'Sales';
+$MESS['GL_AUTHOR'] = 'Author:';
+$MESS['GL_PLACE_CREATION'] = 'place creation:';
+$MESS['GL_CREATION_TIME'] = 'creation time:';
+$MESS['GL_TECHNIQUE'] = 'Technique:';
+$MESS['GL_DIMENSIONS'] = 'Dimensions:';
+$MESS['GL_COPYRIGHT_HOLDER'] = 'Copyright holder:';
+$MESS['GL_CATEGORY'] = 'Category:';
+$MESS['GL_KEYWORDS'] = 'Keywords';
+$MESS['GL_TYPE_SIZE_IMAGE'] = 'The type and size of the image';
+$MESS['GL_DESCRIPTION'] = 'Description';
+$MESS['GL_ADDITIONAL_INFORMATION'] = 'Additional information';
+$MESS['GL_PROVENANCE'] = 'Provenance:';
+$MESS['GL_RESTORATION_WORK'] = 'Restoration works:';
+$MESS['GL_TECHNICAL_CONDITION'] = 'Technical condition:';
+$MESS['GL_CUSTOMER'] = 'Customer:';
+$MESS['GL_MODEL'] = 'Model:';
+$MESS['GL_SKETCHES'] = 'Sketches';
+$MESS['GL_OTHER'] = 'Other:';
--- local/components/glyf/user.picture/templates/picture/lang/ru/template.php
+++ local/components/glyf/user.picture/templates/picture/lang/ru/template.php
@@ -1,5 +1,29 @@
 <?php
 
 $MESS['GL_NO_FOUND'] = 'К сожалению, изображение не найдено.';
-
 $MESS['GL_TYPE_AND_SIZE'] = 'Тип и размер изображения';
+$MESS['GL_EDIT'] = 'Редактировать';
+$MESS['GL_PERSONAL_ACCOUNT'] = 'Личный кабинет';
+$MESS['GL_DIRECTORY'] = 'Каталог';
+$MESS['GL_VIEWS'] = 'просмотров';
+$MESS['GL_VIEWS_2'] = 'Просмотры';
+$MESS['GL_SALES'] = 'продаж';
+$MESS['GL_SALES_2'] = 'Продажи';
+$MESS['GL_AUTHOR'] = 'Автор:';
+$MESS['GL_PLACE_CREATION'] = 'Место создания:';
+$MESS['GL_CREATION_TIME'] = 'Время создания:';
+$MESS['GL_TECHNIQUE'] = 'Техника:';
+$MESS['GL_DIMENSIONS'] = 'Размеры:';
+$MESS['GL_COPYRIGHT_HOLDER'] = 'Правообладатель:';
+$MESS['GL_CATEGORY'] = 'Категория:';
+$MESS['GL_KEYWORDS'] = 'Ключевые слова';
+$MESS['GL_TYPE_SIZE_IMAGE'] = 'Тип и размер изображения';
+$MESS['GL_DESCRIPTION'] = 'Описание';
+$MESS['GL_ADDITIONAL_INFORMATION'] = 'Дополнительная информация';
+$MESS['GL_PROVENANCE'] = 'Провенанс:';
+$MESS['GL_RESTORATION_WORK'] = 'Реставрационные работы:';
+$MESS['GL_TECHNICAL_CONDITION'] = 'Техническое состояние:';
+$MESS['GL_CUSTOMER'] = 'Заказчик:';
+$MESS['GL_MODEL'] = 'Модель:';
+$MESS['GL_SKETCHES'] = 'Эскизы:';
+$MESS['GL_OTHER'] = 'Прочее:';
