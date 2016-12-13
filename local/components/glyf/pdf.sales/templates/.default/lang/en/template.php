cbf90c9670de44493b3c425269645eafd63d1a02
root <root@vps-1045602-16500.host4g.ru>
2016-12-13 18:24:28 +0300

    Translates.

---
+++ local/components/glyf/pdf.sales/templates/.default/lang/en/template.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS['GL_SALES_STATISTICS'] = 'Sales statistics';
+$MESS['GL_NAME'] = 'Name';
+$MESS['GL_PRICE'] = 'Price (RUB)';
+$MESS['GL_DATE_2'] = 'Date';
---
+++ local/components/glyf/pdf.sales/templates/.default/lang/ru/template.php
@@ -0,0 +1,6 @@
+<?php
+
+$MESS['GL_SALES_STATISTICS'] = 'Статистика продаж';
+$MESS['GL_NAME'] = 'Название';
+$MESS['GL_PRICE'] = 'Цена (руб)';
+$MESS['GL_DATE_2'] = 'Дата';
